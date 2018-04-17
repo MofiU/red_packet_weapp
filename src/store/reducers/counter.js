207a6525af965c6648d59ad3cfbf398a6a4c71a7
bo <bo.zeng@laxino.com>
2018-04-17 15:31:10 +0800

    add wifi

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,13 +1,41 @@
 import { handleActions } from 'redux-actions'
-import { INCREMENT } from '../types/counter'
+import { INCREMENT, RANDOM_INCREMENT } from '../types/counter'
 
 export default handleActions({
   [INCREMENT] (state) {
+    const strftime = require('strftime')
+    const transactionAmount = parseFloat((Math.random()* 100).toFixed(2))
+    const reward = parseFloat((transactionAmount * 0.05).toFixed(2))
     return {
       ...state,
-      reward: state.reward + parseFloat((Math.random(10)* 10).toFixed(2))
+      reward: reward,
+      totalReward: state.totalReward + reward,
+      records: [
+        ...state.records,
+        { id: (Math.random()*100000).toFixed(0), reward: reward, created_at: strftime('%m月%d日 %R', new Date(Date.now())),  transactionAmount: transactionAmount}
+      ]
+    }
+  },
+  [RANDOM_INCREMENT] (state) {
+    const strftime = require('strftime')
+    const transactionAmount = parseFloat((Math.random() * 100).toFixed(2))
+    const reward = parseFloat((transactionAmount * (Math.random() * 0.1 + 0.01)).toFixed(2))
+    return {
+      ...state,
+      reward: reward,
+      totalReward: state.totalReward + reward,
+      records: [
+        ...state.records,
+        { id: (Math.random()*100000).toFixed(0), reward: reward, created_at: strftime('%m月%d日 %R', new Date(Date.now())),  transactionAmount: transactionAmount}
+      ]
     }
   }
 }, {
-  reward: 0
+  reward: 0,
+  records: [
+    {id: (Math.random() * 100000).toFixed(0), reward: 3.00, created_at: require('strftime')('%m月%d日 %R', new Date(Date.now())),  transactionAmount: parseFloat((Math.random()* 100).toFixed(2))},
+    {id: (Math.random() * 100000).toFixed(0), reward: 3.00, created_at: require('strftime')('%m月%d日 %R', new Date(Date.now())),  transactionAmount: parseFloat((Math.random()* 100).toFixed(2))},
+    {id: (Math.random() * 100000).toFixed(0), reward: 4.00, created_at: require('strftime')('%m月%d日 %R', new Date(Date.now())),  transactionAmount: parseFloat((Math.random()* 100).toFixed(2))}
+  ],
+  totalReward: 10
 })
--- src/store/types/counter.js
+++ src/store/types/counter.js
@@ -1,2 +1,3 @@
 export const INCREMENT = 'INCREMENT'
 
+export const RANDOM_INCREMENT = 'RANDOM_INCREMENT'
